f7761e488a4572f4e1cba00d1ec4fc333e6611ca
office10 <office10@ucione.local>
2024-05-27 18:45:58 +0200

    dodaj dio

---
+++ script.js
@@ -0,0 +1,8 @@
+function login(){
+    let email = document.getElementById["email"].value;
+    let pass = document.getElementById["pass"].value;
+  if (x == "") {
+    alert("Polja moraju biti popunjena");
+    return false;
+  }
+}
